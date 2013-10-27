d41585e8f8b3ab9d3417309c4355dc913f20fdc4
David Cheng <davidxcheng@gmail.com>
2013-10-27 23:25:19 +0100

    'black fanta' skin + debug logic for utf8octets

--- js/async.js
+++ js/async.js
@@ -1,5 +1,16 @@
 define(['modules/codePointConverter'], function(convert) {
 
+	var binder = function() {
+		var lblOutput = document.getElementById('output');
+		document.getElementById('str')
+			.onkeyup = function(e) {
+				if (!this.value.length) return;
+
+				console.log(convert(this.value.charCodeAt(0)));
+				lblOutput.innerHTML = this.value.charCodeAt(0);
+			};
+	}
+
 	return {
 		init: function() {
 			var codePoints = document.getElementById('alpha')
@@ -9,14 +20,12 @@ define(['modules/codePointConverter'], function(convert) {
 					return letter.charCodeAt(0);
 				});
 
-			document.getElementById('dec').innerHTML = codePoints.map(function(codePoint) {
-				return '<span>' + codePoint + '</span>';
-			}).join('');
-
 			var binary = codePoints.map(function(codePoint) {
-				return  '<span>' + convert.toUtf8BinaryStringRepresentation(codePoint) + '</span>';
+				return  '<span>' + convert(codePoint).join(' ') + '</span>';
 			});
-			document.getElementById('binary').innerHTML = binary.join('');
+			document.getElementById('octets').innerHTML = binary.join('');
+
+			binder();
 		}
 	};
 });
