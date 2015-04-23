b79e03f0951a0a94eab613bffb4e0880f1b94422
David Cheng <davidxcheng@gmail.com>
2015-04-23 14:00:25 +0200

    replace font logo with img

--- js/async.js
+++ js/async.js
@@ -1,8 +1,8 @@
 define(['modules/codePointConverter'], function(convert) {
 	return {
 		init: function() {
-			var codePoints = document.getElementById('alpha')
-				.innerText
+			var codePoints = document.getElementById('logo-img')
+				.alt
 				.split('')
 				.map(function(letter) {
 					return letter.charCodeAt(0);
@@ -11,6 +11,7 @@ define(['modules/codePointConverter'], function(convert) {
 			var binary = codePoints.map(function(codePoint) {
 				return  '<span>' + convert(codePoint).join(' ') + '</span>';
 			});
+
 			document.getElementById('octets').innerHTML = binary.join('');
 		}
 	};
